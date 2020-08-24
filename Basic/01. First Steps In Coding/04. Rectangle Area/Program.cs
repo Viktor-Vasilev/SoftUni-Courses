523fcbd039f93d0f76713be2217a5fbadf2d0723
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2020-08-24 23:56:17 +0300

    First commit

---
+++ Basic/01. First Steps In Coding/04. Rectangle Area/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _04._Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 7;
+            int area = a * b;
+            Console.WriteLine(area);
+        }
+    }
+}
