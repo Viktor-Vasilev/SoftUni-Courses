b06856872352185730faea275d5f685bb0f7a24b
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2020-08-27 02:07:03 +0300

    Fundamentals

---
+++ Fundamentals/Mid Exams/20200812/1. Computer Store/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Collections.Immutable;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _000_boza
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string partPrice = Console.ReadLine();
+
+            double total = 0;
+
+            double currentPrice = 0;
+
+            while (partPrice != "special" && partPrice != "regular")
+            {
+
+
+                currentPrice = double.Parse(partPrice);
+
+                if (currentPrice > 0)
+                {
+                    total += currentPrice;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid price!");
+                }
+
+                partPrice = Console.ReadLine();
+            }
+
+            if (total == 0)
+            {
+                Console.WriteLine("Invalid order!");
+                return;
+            }
+
+            double taxes = total * 0.2;
+
+            Console.WriteLine($"Congratulations you've just bought a new computer!");
+            Console.WriteLine($"Price without taxes: {total:f2}$");
+            Console.WriteLine($"Taxes: {taxes:f2}$");
+            Console.WriteLine("-----------");
+
+            if (partPrice == "regular")
+            {
+                Console.WriteLine($"Total price: {total + taxes:f2}$");
+            }
+            else
+            {
+                double finalTotal = (total + taxes) * 0.9;
+                Console.WriteLine($"Total price: {finalTotal:f2}$");
+            }
+
+
+        }
+
+    }
+}
+
---
+++ Fundamentals/Mid Exams/20200812/2. The Lift Problem/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Collections.Immutable;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _000_boza
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int passengers = int.Parse(Console.ReadLine());
+
+            List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int wagonCapacity = 4;
+
+            for (int i = 0; i < wagons.Count; i++)
+            {
+                while (wagonCapacity - wagons[i] != 0 && passengers > 0)
+                {
+                    passengers--;
+                    wagons[i] += 1;
+                }
+            }
+            if (passengers > 0)
+            {
+                Console.WriteLine($"There isn't enough space! {passengers} people in a queue!");
+                Console.WriteLine(String.Join(" ", wagons));
+            }
+            else if (passengers == 0 && wagons[wagons.Count - 1] == 4)
+            {
+                Console.WriteLine(String.Join(" ", wagons));
+            }
+            else
+            {
+                Console.WriteLine("The lift has empty spots!");
+                Console.WriteLine(String.Join(" ", wagons));
+            }
+
+        }
+
+    }
+}
