426374a9bb5ba23f9aa0c71fecd7b45484aa8a31
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2020-09-17 00:53:30 +0300

    Advanced
    100/100

---
+++ Advanced/C# Advanced/3-4. Stacks And Queues/Lab/3.  Simple Calculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.Lab_3.__Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] expression = Console.ReadLine().Split(' ').Reverse().ToArray();
+
+            //Stack<string> stack = new Stack<string>();
+
+            //for (int i = 0; i < expression.Length; i++)
+            //{
+            //    stack.Push(expression[i]);
+            //}
+
+            Stack<string> stack = new Stack<string>(expression);
+
+            while (stack.Count > 1)
+            {
+                int first = int.Parse(stack.Pop());
+                string sign = (stack.Pop());
+                int second = int.Parse(stack.Pop());
+
+                switch (sign)
+                {
+                    case "+":
+                        stack.Push((first + second).ToString());
+                        break;
+                    case "-":
+                        stack.Push((first - second).ToString());
+                        break;
+                }
+
+            }
+
+            Console.WriteLine(stack.Pop());
+
+        }
+       
+    }
+}
