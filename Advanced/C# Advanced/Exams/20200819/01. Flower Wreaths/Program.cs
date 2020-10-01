a239d9753b87377adada596acda2eeb872b369f8
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2020-10-02 01:05:17 +0300

    Advanced
    Advanced

--- Advanced/C# Advanced/Exams/20200819/01. Flower Wreaths/Program.cs
+++ Advanced/C# Advanced/Exams/20200819/01. Flower Wreaths/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _20200819_01._Flower_Wreaths
 {
@@ -6,10 +8,52 @@ namespace _20200819_01._Flower_Wreaths
     {
         static void Main(string[] args)
         {
-            
+            int[] inputLillies = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] inputRoses = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 
+            Queue<int> roses = new Queue<int>(inputRoses);
+            Stack<int> lillies = new Stack<int>(inputLillies);
 
+            int madeWreaths = 0;
 
+            int storedFlowersSum = 0;
+
+            while (roses.Any() && lillies.Any())
+            {
+                int currentRose = roses.Peek();
+                int currentLillie = lillies.Peek();
+
+                int currentSum = currentLillie + currentRose;
+
+                if (currentSum == 15)
+                {
+                    lillies.Pop();
+                    roses.Dequeue();
+                    madeWreaths++;
+                }
+                else if (currentSum < 15)
+                {
+                    lillies.Pop();
+                    roses.Dequeue();
+                    storedFlowersSum += currentSum;
+                }
+                else if (currentSum > 15)
+                {
+                    lillies.Push(lillies.Pop() - 2);
+                }
+
+            }
+
+            madeWreaths += (storedFlowersSum / 15);
+
+            if (madeWreaths >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {madeWreaths} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - madeWreaths} wreaths more!");
+            }
 
 
         }
---
+++ Advanced/C# Advanced/Exams/20200819/02. Bee/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace _20200819_02._Bee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] beeTeritory = new char[n,n];
+
+            int beeRow = -1;
+            int beeCol = -1;
+
+            for (int row = 0; row < n; row++)
+            {
+                var input = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    beeTeritory[row, col] = input[col];
+
+                    if (input[col] == 'B')
+                    {   beeRow = row;
+                        beeCol = col;
+                    }
+                }                
+
+            }
+            int polinatedFlowers = 0;
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                beeTeritory[beeRow, beeCol] = '.'; // маркираме сегашното ни местоположение с .
+                
+                if (command == "up")  // местим пчелата
+                {
+                    beeRow--;
+                }
+                else if (command == "down")
+                {
+                    beeRow++;
+                }
+                else if (command == "left")
+                {
+                    beeCol--;
+                }
+                else if (command == "right")
+                {
+                    beeCol++;
+                }
+
+
+                if (beeCol > n -1 || beeCol < 0 || beeRow < 0 || beeRow > n -1) // проверяваме дали не сме излезли
+                {
+                    Console.WriteLine("The bee got lost!");
+                    break;
+                }
+
+                if (beeTeritory[beeRow, beeCol] == 'f') // ако срещнем цвете
+                {
+                    polinatedFlowers++;
+                }
+                else if (beeTeritory[beeRow, beeCol] == 'O') // ако срещнем бонус
+                {
+                    continue;
+                }
+
+
+                command = Console.ReadLine();
+            }
+
+            if (command == "End") // казваме къде е пчелата!!!!!!!
+            {
+                beeTeritory[beeRow, beeCol] = 'B'; 
+            }
+
+            if (polinatedFlowers < 5) // проверяваме дали е успешна
+            {
+                Console.WriteLine($"The bee couldn't pollinate the flowers, she needed {5 - polinatedFlowers} flowers more");
+            }
+            else
+            {
+                Console.WriteLine($"Great job, the bee managed to pollinate {polinatedFlowers} flowers!");
+            }
+
+            
+
+            for (int row = 0; row < n; row++) // отпечатваме
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(beeTeritory[row, col]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
