1ab059c270ca517b54d1e00f46fa1d1f1d9feb01
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2020-10-06 00:56:51 +0300

    Advanced
    Advanced

---
+++ Advanced/C# Advanced/Exams/20191023/20191023 02. Make A Salad/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _20191023_02._Make_A_Salad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+
+            string[] inputVegetables = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int[] inputSalads = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Queue<string> vegetables = new Queue<string>(inputVegetables);
+
+            Stack<int> salads = new Stack<int>(inputSalads);
+
+            List<int> madeSalads = new List<int>();
+
+            int leftCalories = 0;
+
+            while (vegetables.Any() && salads.Any())
+            {
+                string currentVegetable = vegetables.Dequeue();
+
+                int currentSalad = salads.Peek();
+                
+                if (currentSalad == 0)
+                {
+                    continue;
+                }
+
+                int currentCalories = 0;
+
+                if (currentVegetable == "tomato")
+                {
+                    currentCalories = 80;
+                }
+                else if (currentVegetable == "carrot")
+                {
+                    currentCalories = 136;
+                }
+                else if (currentVegetable == "lettuce")
+                {
+                    currentCalories = 109;
+                }
+                else if (currentVegetable == "potato")
+                {
+                    currentCalories = 215;
+                }
+
+
+                if (leftCalories == 0)
+                {
+                    leftCalories = currentSalad - currentCalories;
+
+                    if (currentCalories >= currentSalad)
+                    {
+                        madeSalads.Add(currentSalad);
+
+                        salads.Pop();
+
+                        leftCalories = 0;
+                    }
+                }
+                else
+                {
+                    if (currentCalories >= leftCalories)
+                    {
+                        madeSalads.Add(currentSalad);
+
+                        salads.Pop();
+
+                        leftCalories = 0;
+                    }
+                    else
+                    {
+                        leftCalories = leftCalories - currentCalories;
+                    }
+                }
+            }
+
+            if (leftCalories > 0 && salads.Any())
+            {
+                madeSalads.Add(salads.Pop());
+            }
+
+            Console.WriteLine(string.Join(" ", madeSalads));
+
+            if (vegetables.Any())
+            {
+                Console.WriteLine(string.Join(" ", vegetables));
+            }
+            else if (salads.Any())
+            {
+                Console.WriteLine(string.Join(" ", salads));
+            }
+
+        }
+    }
+}
