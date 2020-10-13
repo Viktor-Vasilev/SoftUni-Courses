ed1798923af7e95de85b0c016f440d7eb30e9d30
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2020-10-14 00:14:37 +0300

    Advanced
    Advanced

---
+++ Advanced/C# Advanced/17-18. Generics/Lab/02. Generic Array Creator/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            string[] array = ArrayCreator.Create(5, "Pesho");
+            
+            Console.WriteLine(String.Join(", ", array));
+
+            int[] integers = ArrayCreator.Create(10, 33);
+
+            Console.WriteLine(String.Join(", ", integers));
+
+        }
+    }
+}
---
+++ Advanced/C# Advanced/17-18. Generics/Lab/03. Generic Scale/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var scale = new EqualityScale<int>(5, 6);
+            Console.WriteLine(scale.AreEqual());
+
+            var scale2 = new EqualityScale<int>(5, 5);
+            Console.WriteLine(scale2.AreEqual());
+
+
+
+
+        }
+    }
+}
