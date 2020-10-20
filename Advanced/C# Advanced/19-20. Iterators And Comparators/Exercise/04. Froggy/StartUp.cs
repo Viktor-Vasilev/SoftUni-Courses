ab42c8db9b9e27a738085d61fea69f6c58af2523
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2020-10-20 23:36:42 +0300

    Advanced
    Advanced

---
+++ Advanced/C# Advanced/19-20. Iterators And Comparators/Exercise/04. Froggy/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+
+namespace IteratorsAndComparators
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] stones = Console.ReadLine()
+                                 .Split(", ")
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            Lake lake = new Lake(stones);
+            Console.WriteLine(String.Join(", ", lake));
+
+
+
+        }
+    }
+}
