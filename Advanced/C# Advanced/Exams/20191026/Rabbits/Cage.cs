19884bcbc2cec83d8c6b6082731ecf48ba1fd6c6
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2020-10-23 01:08:03 +0300

    Advanced
    Advanced

---
+++ Advanced/C# Advanced/Exams/20191026/Rabbits/Cage.cs
@@ -0,0 +1,84 @@
+﻿namespace Rabbits
+{
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using System.Text;
+
+    public class Cage
+    {
+
+        private List<Rabbit> data;
+
+        public Cage(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Rabbit>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count => data.Count;
+
+        public void Add(Rabbit rabbit)
+        {
+            if (Capacity > Count)
+            {
+                data.Add(rabbit);
+            }
+        }
+
+        public bool RemoveRabbit(string name)
+        {
+            Rabbit rabbitToRemove = data.FirstOrDefault(x => x.Name == name);
+            return data.Remove(rabbitToRemove);
+        
+        }
+
+        public void RemoveSpecies(string species)
+        {
+            data.RemoveAll(r => r.Species == species);
+             
+        }
+
+        public Rabbit SellRabbit(string name)
+        {
+            Rabbit rabbit = data.FirstOrDefault(r => r.Name == name);
+
+            if (rabbit != null)
+            {
+                rabbit.Available = false;
+            }
+
+            return rabbit;
+        
+        }
+
+        public Rabbit[] SellRabbitsBySpecies(string species)
+        {
+            Rabbit[] rabbits = data.Where(r => r.Species == species).ToArray();
+
+            foreach (var rabbit in rabbits)
+            {
+                rabbit.Available = false;
+            }
+            return rabbits;
+        }
+
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Rabbits available at {Name}:");
+            foreach (var rabbit in data.Where(r => r.Available))
+            {
+                sb.AppendLine(rabbit.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
