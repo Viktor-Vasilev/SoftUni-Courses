76269fe851d7ca9dc1abcee470e50848706d23dc
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2020-10-26 19:05:40 +0200

    Advanced
    Advanced

--- Advanced/C# Advanced/23. Exam/1 Exam Problem 3 - Classroom/Classroom.cs
+++ Advanced/C# Advanced/23. Exam/1 Exam Problem 3 - Classroom/Classroom.cs
@@ -7,81 +7,68 @@ namespace ClassroomProject
 {
     public class Classroom
     {
+        private List<Student> students = new List<Student>();
 
-        List<Student> students;
-        
-
-        public int Capacity { get; set; }
-
-        public int Count
-        {
-            get
-            {
-                return this.students.Count;
-            }
-        }
         public Classroom(int capacity)
         {
-           
-            Capacity = capacity;          
+            Capacity = capacity;
             students = new List<Student>();
         }
 
-       
+        public int Capacity { get; set; }
+        public int Count { get { return students.Count; } }
 
-        public string RegisterStudent(Student student) ///////////////// ?????????????
+        public string RegisterStudent(Student student)
         {
-            if (Capacity > Count)
+            if (students.Count < Capacity)
             {
                 students.Add(student);
                 return $"Added student {student.FirstName} {student.LastName}";
             }
-            
             else
             {
                 return "No seats in the classroom";
             }
         }
 
-        public string DismissStudent(string firstName, string lastName)  ///////////////// ?????????????
+        public string DismissStudent(string firstName, string lastName)
         {
-            
-            
-            if (students.Any(x => x.FirstName == firstName && x.LastName == lastName))
+            Student realStudent = students.FirstOrDefault(s => s.FirstName == firstName && s.LastName == lastName);
+            if (realStudent == null)
             {
-                Student studentToRemove = students.Where(x => x.FirstName == firstName && x.LastName == lastName).FirstOrDefault();
-                return $"Dismissed student {firstName} {lastName}";
-                students.Remove(studentToRemove);
+                return "Student not found";
             }
             else
             {
-                return "Student not found";
+                students.Remove(realStudent);
+                return $"Dismissed student {realStudent.FirstName} {realStudent.LastName}";
             }
         }
 
         public string GetSubjectInfo(string subject)
         {
-            
-            
+            List<Student> studentsSelect = students.Where(s => s.Subject == subject).ToList();
+            if (studentsSelect.Count == 0)
+            {
+                return "No students enrolled for the subject";
+            }
+
             StringBuilder sb = new StringBuilder();
             sb.AppendLine($"Subject: {subject}");
             sb.AppendLine("Students:");
-            foreach (var item in students.Where(x => x.Subject == subject))
+            foreach (Student student in studentsSelect)
             {
-                sb.AppendLine($"{item.FirstName} {item.LastName}");
+                sb.AppendLine($"{student.FirstName} {student.LastName}");
             }
-
             return sb.ToString().TrimEnd();
         }
 
+        public int GetStudentsCount()
+        {
+            return Count;
+        }
 
-
-        public int GetStudentsCount => students.Count;
-
-        public Student GetStudent(string firstName, string lastName) => students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
-
-        
-        
-
-}
+        public Student GetStudent(string firstName, string lastName)
+         => students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+    }
 }
