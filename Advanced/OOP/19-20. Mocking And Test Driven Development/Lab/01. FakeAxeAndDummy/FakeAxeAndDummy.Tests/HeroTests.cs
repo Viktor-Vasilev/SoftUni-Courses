55d0f7fa503950b5caf74d2c5273346ef6d38f3a
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2020-12-26 21:51:02 +0200

    OOP
    OOP

---
+++ Advanced/OOP/19-20. Mocking And Test Driven Development/Lab/01. FakeAxeAndDummy/FakeAxeAndDummy.Tests/HeroTests.cs
@@ -0,0 +1,43 @@
+﻿using FakeAxeAndDummy.Contracts;
+using FakeAxeAndDummy.Tests.Fakes;
+using Moq;
+using NUnit.Framework;
+
+[TestFixture]
+public class HeroTests
+{
+    [Test]
+    public void GivenHero_WhenAttackedTargetDies_ThenHeroReceivesExperience()
+    {
+        IAttackable fakeTarget = new IAttackableFake();
+        IWeapon fakeWeapon = new IWeaponFake();
+
+        Hero hero = new Hero("Pesho", fakeWeapon);
+
+        hero.Attack(fakeTarget);
+
+        Assert.AreEqual(hero.Experience, 20);
+
+    }
+
+    [Test]
+    public void WITHMock_GivenHero_WhenAttackedTargetDies_ThenHeroReceivesExperience()
+    {
+        Mock<IAttackable> fakeTarget = new Mock<IAttackable>();
+        Mock<IWeapon> fakeWeapon = new Mock<IWeapon>();
+
+        fakeTarget.Setup(f => f.GiveExperience()).Returns(20);
+        fakeTarget.Setup(f => f.IsDead()).Returns(true);
+
+        Hero hero = new Hero("Pesho", fakeWeapon.Object);
+
+        hero.Attack(fakeTarget.Object);
+
+        Assert.AreEqual(hero.Experience, 20);
+
+    }
+
+
+
+
+}
---
+++ Advanced/OOP/19-20. Mocking And Test Driven Development/Lab/01. FakeAxeAndDummy/FakeAxeAndDummy/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using FakeAxeAndDummy.Contracts;
+using Moq;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        //IWeapon weapon = new Axe(10, 10);
+
+        //IAttackable target = new Dummy(10, 20);
+
+        //Hero hero = new Hero("Pesho", weapon);
+
+        //hero.Attack(target);
+
+        //Mock<IAttackable> target = new Mock<IAttackable>();
+        //target.Setup(t => t.GiveExperience()).Returns(20);
+
+
+        //Mock<IWeapon> weapon = new Mock<IWeapon>();
+        //weapon.Setup(w => w.AttackPoints).Returns(55);
+
+
+        
+
+        //Hero hero = new Hero("Pesho", weapon.Object);
+
+    }
+}
