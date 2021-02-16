092658289ba198dd71891d2b29a81c329366164d
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2021-02-16 20:57:35 +0200

    EFC
    EFC

---
+++ C# DB/Entity Framework Core/3-4. ADO.NET/Lab.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection(
+                @"Server=.\SQLEXPRESS;Integrated Security=true;Database=SoftUni"))
+            {
+                connection.Open();
+
+                //// Execute NonQuery
+                //SqlCommand sqlCommand = new SqlCommand(
+                //    "UPDATE Employees SET Salary = Salary + 0.12 WHERE FirstName LIKE 'N%'", connection);
+                //var rowsAffected = sqlCommand.ExecuteNonQuery();
+                //Console.WriteLine(rowsAffected);
+
+
+
+                //// Execute Scalar
+                //SqlCommand sqlCommand = new SqlCommand(
+                //    "SELECT COUNT(*) FROM Employees", connection);
+                //var rowsAffected = (int)sqlCommand.ExecuteScalar();
+                //Console.WriteLine(rowsAffected);
+
+
+
+                // Execite Reader
+                var sqlCommand = new SqlCommand(
+                    "SELECT * FROM Employees ORDER BY [FirstName]", connection);
+                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                {
+                    while (sqlDataReader.Read())
+                    {
+                        Console.WriteLine(sqlDataReader["FirstName"]);
+                    }
+                }
+            }
+        }
+    }
+}
