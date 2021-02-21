fb3c3bb855ee8bba4ad57a65c73affce942ce072
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2021-02-21 10:27:48 +0200

    EFC
    EFC

---
+++ C# DB/Entity Framework Core/5-6. ORM Fundamentals/Lab/Program.cs
@@ -0,0 +1,35 @@
+﻿using Lab.Models;
+using System;
+using System.Linq;
+
+namespace Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // NuGet Packages
+            // Microsoft.EntityFrameworkCore.SqlServer
+            // Microsoft.EntityFrameworkCore.Design
+
+            // dotnet ef dbcontext scaffold "Server=.\SQLEXPRESS;Integrated Security=true;Database=SoftUni" Microsoft.EntityFrameworkCore.SqlServer -o Models
+
+            var db = new SoftUniContext();
+
+            
+            Console.WriteLine(db.Employees.Count());
+
+            var employees = db.Employees.Where(x => x.FirstName.StartsWith("N")).OrderByDescending(x => x.Salary)
+                .Select(x => new { x.FirstName, x.LastName, x.Salary }).ToList();
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName} => {employee.Salary}");
+            }
+
+
+
+
+        }
+    }
+}
