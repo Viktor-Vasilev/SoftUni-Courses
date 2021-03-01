0e1a79f28fcf9d342e99c4f45e0a3542f0fd9abc
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2021-03-01 18:25:35 +0200

    EFC
    EFC

---
+++ C# DB/Entity Framework Core/9-10. Entity Relations/Exercise/P03_FootballBetting/Data/Models/Team.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Team
+    {
+        public Team()
+        {
+            this.Players = new HashSet<Player>();
+            this.HomeGames = new HashSet<Game>();
+            this.AwayGames = new HashSet<Game>();
+        }
+
+        public int TeamId { get; set; }
+
+        public string Name { get; set; }
+
+        public string LogoUrl { get; set; }
+
+        public string Initials { get; set; }
+
+        public decimal Budget { get; set; }
+
+        public int PrimaryKitColorId { get; set; } // Foreign Key
+        public Color PrimaryKitColor { get; set; } // Navigational property
+
+        public int SecondaryKitColorId { get; set; } // Foreign Key
+        public Color SecondaryKitColor { get; set; } // Navigational property
+
+        public int TownId { get; set; } // Foreign Key
+        public Town Town { get; set; } // Navigational property
+
+        public ICollection<Player> Players { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public ICollection<Game> HomeGames { get; set; }
+
+        public ICollection<Game> AwayGames { get; set; }
+
+        //collection of players
+        //collection of games
+
+    }
+}
---
+++ C# DB/Entity Framework Core/9-10. Entity Relations/Exercise/P03_FootballBetting/Program.cs
@@ -0,0 +1,18 @@
+﻿using P03_FootballBetting.Data;
+using System;
+
+namespace P03_FootballBetting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var context = new FootballBettingContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+
+        }
+    }
+}
