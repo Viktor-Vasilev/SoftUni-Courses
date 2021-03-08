bac8e2c9bf22845b87bcd56f771ddbd87b0ae57d
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2021-03-08 23:29:27 +0200

    EFC
    EFC

---
+++ C# DB/Entity Framework Core/15-16. C# Auto Mapping Objects/Lab/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using Lab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using System.Text;
+
+
+
+namespace Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // .ReverseMap() се ползва когато искаме от вю модел да направиме клас, който после да вкараме в базата. 
+            // Примерно когато потребителя е въвел нова песен от приложението, тя после за да се вкара в базата трябва да се обърне на клас, който го има в db контекста.
+
+            //dotnet ef dbcontext scaffold "Server=.\SQLEXPRESS;Database=MusicX;Integrated Security=true" Microsoft.EntityFrameworkCore.SqlServer -o Models
+
+          
+
+            var config = new MapperConfiguration(config =>
+            {
+                config.CreateMap<Song, SongNameDTO>();
+            });
+
+            var mapper = config.CreateMapper();
+
+            var db = new MusicXContext();
+
+            Song song = db.Songs.Where(x => x.Id == 4)
+              .FirstOrDefault();
+
+            var songDTO = mapper.Map<SongNameDTO>(song);
+            Console.WriteLine(songDTO);
+
+          
+
+
+
+        }
+
+        class SongNameDTO
+        {
+            public string Name { get; set; }
+        }
+}
+}
