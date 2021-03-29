df3221bdda843c328f20dd6b491a81a1d4b7f10c
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2021-03-30 02:56:11 +0300

    EFC
    EFC

---
+++ C# DB/Entity Framework Core/27. Exam Preparation/DataProcessor/Dto/Export/PurchaseXmlExportModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+using VaporStore.Data.Models;
+
+namespace VaporStore.DataProcessor.Dto.Export
+{
+    [XmlType("Purchase")]
+    public class PurchaseXmlExportModel
+    {
+        public string Card { get; set; }
+        public string Cvc { get; set; }
+
+        public string Date { get; set; }
+
+        public GameXmlExportModel Game  { get; set; }
+
+
+    }
+}
---
+++ C# DB/Entity Framework Core/27. Exam Preparation/DataProcessor/Dto/Import/GameJsonImportModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace VaporStore.DataProcessor.Dto.Import
+{
+   
+    public class GameJsonImportModel
+    {
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Developer { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+
+        public IEnumerable<string> Tags { get; set; }
+
+    }
+}
