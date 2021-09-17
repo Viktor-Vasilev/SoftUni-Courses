836b4654aec8042e986409327807414045d04768
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2021-09-18 00:15:49 +0300

    js
    js

--- Javascript/5. Arrays And Nested Arrays/8. Process Odd Positions.js
+++ Javascript/5. Arrays And Nested Arrays/8. Process Odd Positions.js
@@ -10,3 +10,14 @@ console.log(doubled.join(' '));
 
 oddPositions([10, 15, 20, 25]);
 oddPositions([3, 0, 10, 4, 7, 3]);
+
+// function oddPositionsChained(numbers){
+// console.log(numbers
+//     .filter((v, i) => i % 2 == 1)
+//     .map(x => x * 2)
+//     .reverse()
+//     .join(' '));
+// }
+
+// oddPositionsChained([10, 15, 20, 25]);
+// oddPositionsChained([3, 0, 10, 4, 7, 3]);
