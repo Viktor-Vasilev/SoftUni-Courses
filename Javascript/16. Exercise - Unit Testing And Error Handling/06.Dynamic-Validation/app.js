9a27c0063d9b2f008cc705c2fb2a1da81f89b0dd
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2021-10-16 00:30:13 +0300

    js
    js

---
+++ Javascript/16. Exercise - Unit Testing And Error Handling/06.Dynamic-Validation/app.js
@@ -0,0 +1,13 @@
+function validate() {
+    const inputField = document.getElementById("email");
+
+    const validMailPattern = /(^[a-z]+@[a-z]+\.[a-z]+$)/;
+
+    inputField.addEventListener("change", () => {
+        if(!validMailPattern.test(inputField.value)) {
+            inputField.classList.add("error");
+        } else {
+            inputField.classList.remove("error");
+        }
+    });
+}
