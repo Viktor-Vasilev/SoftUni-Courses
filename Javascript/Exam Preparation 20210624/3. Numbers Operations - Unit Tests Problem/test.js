f04e79e29165560fa2eef51d97d9d602dce7d0ea
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2021-10-22 02:30:24 +0300

    js
    js

--- Javascript/Exam Preparation 20210624/3. Numbers Operations - Unit Tests Problem/test.js
+++ Javascript/Exam Preparation 20210624/3. Numbers Operations - Unit Tests Problem/test.js
@@ -1,150 +1,60 @@
 const numberOperations = require('./numberOperations');
-const {expect} = require('chai');
+const {assert} = require('chai');
 
-describe("Repository", function () {
-    describe("constructor", function () {
-        it("Should set initialize with valid data", function () {
-            let props = { name: 'string', age: 'number'};
-            let rep = new Repository(props);
-            expect(rep.props).to.eql(props);
-            expect(rep.data.size).to.equal(0);
-            expect(rep.nextId()).to.equal(0);
- 
-            expect(rep instanceof Repository);
-        });
-    });
- 
-    describe("count", function () {
-        it("Should reflect correct count", function () {
-            let props = { name: 'string', age: 'number'};
-            let rep = new Repository(props);
-            let obj1 = { name: 'ivan', age: 20};
- 
-            expect(rep.count).to.equal(0);
-            rep.add(obj1);
-            expect(rep.count).to.equal(1);
-            rep.update(0, obj1);
-            expect(rep.count).to.equal(1);
-            rep.del(0);
-            expect(rep.count).to.equal(0);
-        });
-    });
- 
-    describe("add", function () {
-        it("Should reflect add entity correctly", function () {
-            let props = { name: 'string', age: 'number'};
-            let rep = new Repository(props);
-            let obj1 = { name: 'ivan', age: 20};
-            let obj2 = { name: 'ivan2', age: 22};
-            let id = rep.add(obj1);
-            expect(id).to.equal(0);
-            let id2 = rep.add(obj2);
-            expect(id2).to.equal(1);
- 
-            let actual = rep.data.get(id2);
-            expect(actual).to.eql(obj2);
-        });
- 
-        it("Should throw when props are not matched", function () {
-            let props = { name: 'string', age: 'number'};
-            let rep = new Repository(props);
-            let obj1 = { name: 'ivan'};
-            let obj2 = { name: 'ivan2', age: true, };
-            let obj3 = { name: 'ivan3', age: 22, hobby: true };
-            expect(()=> rep.add(obj1)).to.throw(Error, `Property age is missing from the entity!`);
-            expect(()=> rep.add(obj2)).to.throw(TypeError, `Property age is not of correct type!`);
-            expect(()=> rep.add(obj3)).to.throw(TypeError, `Property hobby is not of correct type!`);
-        });
-    });
- 
-    describe("getId", function () {
-        it("Should reflect getId entity correctly", function () {
-            let props = { name: 'string', age: 'number'};
-            let rep = new Repository(props);
-            let obj1 = { name: 'ivan', age: 20};
-            let obj2 = { name: 'ivan2', age: 22};
-            let id = rep.add(obj1);
-            let id2 = rep.add(obj2);
- 
-            let actual = rep.getId(1);
-            expect(actual).to.eql(obj2);
-            let actual2 = rep.getId(0);
-            expect(actual2).to.eql(obj1);
-        });
- 
-        it("Should throw when id does not exist", function () {
-            let props = { name: 'string', age: 'number'};
-            let rep = new Repository(props);
-            let obj1 = { name: 'ivan', age: 12};
-            expect(()=> rep.getId(0)).to.throw(Error, `Entity with id: 0 does not exist!`);
-            let id = rep.add(obj1);
-            expect(()=> rep.getId(1)).to.throw(Error, `Entity with id: 1 does not exist!`);
-        });
-    });
- 
-    describe("update", function () {
-        it("Should reflect update entity correctly", function () {
-            let props = { name: 'string', age: 'number'};
-            let rep = new Repository(props);
-            let obj1 = { name: 'ivan', age: 20};
-            let obj2 = { name: 'ivan2', age: 22};
-            let obj3 = { name: 'gosho', age: 15};
-            let obj4 = { name: 'peter', age: 40};
-            let id = rep.add(obj1);
-            let id2 = rep.add(obj2);
- 
-            rep.update(1, obj3);
-            expect(rep.data.get(1)).to.eql(obj3);
-            rep.update(0, obj4);
-            expect(rep.data.get(0)).to.eql(obj4);
-        });
- 
-        it("Should throw when when parameters are incorrect", function () {
-            let props = { name: 'string', age: 'number'};
-            let rep = new Repository(props);
-            let obj1 = { name: 'ivan', age: 12};
-            let obj2 = { name: 'ivan'};
-            let obj3 = { name: 'ivan', age: true};
-            let obj4 = { name: 'ivan', age: 12, hobby: true};
-            expect(()=> rep.update(0, obj1)).to.throw(Error, `Entity with id: 0 does not exist!`);
-            let id = rep.add(obj1);
-            expect(()=> rep.update(1, obj1)).to.throw(Error, `Entity with id: 1 does not exist!`);
- 
-            expect(()=> rep.update(0,obj2)).to.throw(Error, `Property age is missing from the entity!`);
-            expect(()=> rep.update(0,obj3)).to.throw(TypeError, `Property age is not of correct type!`);
-            expect(()=> rep.update(0,obj4)).to.throw(TypeError, `Property hobby is not of correct type!`);
-        });
-    });
- 
-    describe("del", function () {
-        it("Should reflect delete correctly", function () {
-            let props = { name: 'string', age: 'number'};
-            let rep = new Repository(props);
-            let obj1 = { name: 'ivan', age: 20};
-            let obj2 = { name: 'ivan2', age: 22};
-            let obj3 = { name: 'gosho', age: 15};
-            let obj4 = { name: 'peter', age: 40};
-            let id = rep.add(obj1);
-            let id2 = rep.add(obj2);
- 
-            rep.del(0);
-            expect(rep.data.get(1)).to.eql(obj2);
-            rep.del(1);
-            expect(rep.data.size).to.equal(0);
-        });
- 
-        it("Should throw when parameters are incorrect", function () {
-            let props = { name: 'string', age: 'number'};
-            let rep = new Repository(props);
-            let obj1 = { name: 'ivan', age: 12};
-            let obj2 = { name: 'ivan'};
-            let obj3 = { name: 'ivan', age: true};
-            let obj4 = { name: 'ivan', age: 12, hobby: true};
-            expect(()=> rep.del(0)).to.throw(Error, `Entity with id: 0 does not exist!`);
-            let id = rep.add(obj1);
-            expect(()=> rep.del(1)).to.throw(Error, `Entity with id: 1 does not exist!`);
-        });
+describe("powNumber", function () {
+    it("Should return number to the second power", function () {
+        assert.strictEqual(numberOperations.powNumber(3), 9);
+        assert.strictEqual(numberOperations.powNumber(1.5), 1.5 * 1.5);
+        assert.strictEqual(numberOperations.powNumber(-4), 16);
+        assert.strictEqual(numberOperations.powNumber(0), 0);
+        assert.isNaN(numberOperations.powNumber(NaN));
+        // expect(numberOperations.powNumber(NaN)).to.be.NaN;
     });
 });
 
-  
+describe("numberChecker", function () {
+    it("Should correctly coerce input argument", function () {
+        assert.strictEqual(numberOperations.numberChecker('3'), 'The number is lower than 100!');
+        assert.strictEqual(numberOperations.numberChecker(''), 'The number is lower than 100!');
+    });
+
+    it("Should throw when passed a argument that coerces to NaN", function () {
+        assert.throw(() => numberOperations.numberChecker('abc'), Error, 'The input is not a number!');
+        assert.throw(() => numberOperations.numberChecker(undefined), Error, 'The input is not a number!');
+        assert.throw(() => numberOperations.numberChecker('123b'), Error, 'The input is not a number!');
+        // expect(() => numberOperations.numberChecker('123b')).to.throw(Error, 'The input is not a number!');
+    });
+
+    it("Should return correct string when argument is a valid number", function () {
+        assert.strictEqual(numberOperations.numberChecker('99'), 'The number is lower than 100!');
+        assert.strictEqual(numberOperations.numberChecker('-100'), 'The number is lower than 100!');
+        assert.strictEqual(numberOperations.numberChecker('100'), 'The number is greater or equal to 100!');
+        assert.strictEqual(numberOperations.numberChecker('123.51'), 'The number is greater or equal to 100!');
+    });
+});
+
+describe("sumArrays", function () {
+    it("Should return empty array when called with empty arrays", function () {
+        assert.deepEqual(numberOperations.sumArrays([], []), []);
+    });
+
+    it("Should return empty array when one parameter is an empty array", function () {
+        assert.deepEqual(numberOperations.sumArrays([1, 2, 3], []), [1, 2, 3]);
+        assert.deepEqual(numberOperations.sumArrays([], [1, 2, 3]), [1, 2, 3]);
+    });
+
+    it("Should return correct result when both parameters are non empty equal length arrays", function () {
+        assert.deepEqual(numberOperations.sumArrays([1, 2, 3], [2, 3, 4]), [3, 5, 7]);
+        assert.deepEqual(numberOperations.sumArrays([-1, 2, 3], [-2, 3, 4]), [-3, 5, 7]);
+        assert.deepEqual(numberOperations.sumArrays([1.1, 2, 3], [2.2, 3, 4]), [1.1 + 2.2, 5, 7]);
+        assert.deepEqual(numberOperations.sumArrays(['a', 'b', 'c'], ['b', 'c', 'd']), ['ab', 'bc', 'cd']);
+    });
+
+    it("Should return correct result when both parameters are non empty arrays of different length", function () {
+        assert.deepEqual(numberOperations.sumArrays([1, 2, 7], [2, 3]), [3, 5, 7]);
+        assert.deepEqual(numberOperations.sumArrays([-1, 2, 7], [-2, 3]), [-3, 5, 7]);
+        assert.deepEqual(numberOperations.sumArrays([1.1, 2, 8], [2.2, 3]), [1.1 + 2.2, 5, 8]);
+        assert.deepEqual(numberOperations.sumArrays(['a', 'b', 'c'], ['b', 'c']), ['ab', 'bc', 'c']);
+
+    });
+});
