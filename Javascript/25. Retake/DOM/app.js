77ddebb253c7ad4c1d098624b90241e50a16a62f
Viktor Vasilev <70168865+Viktor-Vasilev@users.noreply.github.com>
2022-04-06 21:46:23 +0300

    js
    js

--- Javascript/25. Retake/DOM/app.js
+++ Javascript/25. Retake/DOM/app.js
@@ -2,86 +2,99 @@ window.addEventListener("load", solve);
 
 function solve() {
 
-  let inputTitle = document.getElementById('post-title');
-  let inputCategory = document.getElementById('post-category');
-  let inputContent = document.getElementById('post-content');
-  let btnPublish = document.getElementById('publish-btn');
-  let btnClear = document.getElementById('clear-btn');
+    let title = document.getElementById('post-title');
+    let category = document.getElementById('post-category');
+    let content = document.getElementById('post-content');
+    let btnPublish = document.getElementById('publish-btn');
+    let btnClear = document.getElementById('clear-btn');
 
-  let postForReview = document.getElementById('review-list');
-  let publishedList = document.getElementById('published-list');
+    let postForReview = document.getElementById('review-list');
+    let publishedList = document.getElementById('published-list');
 
-  btnPublish.addEventListener('click', postContent);
+    // let holdData = { title: `${title.value}`, category: `${category.value}`, content: `${content.value}` };
+    btnPublish.addEventListener('click', postContent);
 
-  function postContent(ev) {
-      ev.preventDefault();
+    function postContent(ev) {
+        ev.preventDefault();
 
-      if (inputTitle.value.trim() && inputCategory.value.trim() && inputContent.value.trim()) {
 
-          let li = document.createElement('li');
-          li.className = 'rpost';
+        if (title.value.trim() && category.value.trim() && content.value.trim()) {
+            // btnPublish.style.display = 'none'; //////////////////
 
-          let article = makeArticleElement(inputTitle, inputCategory, inputContent);
-          li.appendChild(article);
+            let li = document.createElement('li');
+            li.className = 'rpost';
 
-          let btnApprove = document.createElement('button');
-          btnApprove.className = 'action-btn approve';
-          btnApprove.textContent = 'APPROVE';
-          article.appendChild(btnApprove);
+            let article = makeArticleElement(title, category, content);
+            li.appendChild(article);
 
-          let btnEdit = document.createElement('button');
-          btnEdit.className = 'action-btn edit';
-          btnEdit.textContent = 'EDIT';
-          article.appendChild(btnEdit);
+            let btnApprove = document.createElement('button');
+            btnApprove.className = 'action-btn approve';
+            btnApprove.textContent = 'APPROVE';
+            article.appendChild(btnApprove);
 
-          postForReview.appendChild(li);
+            let btnEdit = document.createElement('button');
+            btnEdit.className = 'action-btn edit';
+            btnEdit.textContent = 'EDIT';
+            article.appendChild(btnEdit);
 
-          btnEdit.addEventListener('click', (e) => {
-              e.target.parentElement.parentElement.remove();
+            postForReview.appendChild(li);
+            document.querySelector
+                //2.Edit information
+            btnEdit.addEventListener('click', (e) => {
+                let input = e.target.parentElement;
+                let ta = input.querySelector('h4');
+                let cat = input.querySelectorAll('p')[0];
+                let co = input.querySelectorAll('p')[1];
 
-          });
+                title.value = ta.textContent;
+                category.value = cat.textContent.replace('Category: ', '');
+                content.value = co.textContent.replace('Content: ', '');
 
-          btnApprove.addEventListener('click', (e) => {
+                e.target.parentElement.parentElement.remove();
 
-              let liRpost = document.createElement('li');
-              liRpost.className = 'rpost';
+            });
 
-              let artElement = makeArticleElement(inputTitle, inputCategory, inputContent);
-              liRpost.appendChild(artElement);
+            btnApprove.addEventListener('click', (e) => {
 
-              publishedList.appendChild(liRpost);
+                let element = e.target.parentElement.parentElement;
+                let rr = element.querySelectorAll('button');
+                rr[0].remove();
+                rr[1].remove();
 
-              e.target.parentElement.parentElement.remove();
-              inputTitle.value = '';
-              inputCategory.value = '';
-              inputContent.value = '';
+                publishedList.appendChild(element);
+                // e.target.parentElement.parentElement.remove();
+            });
 
-          });
+            btnClear.addEventListener('click', (e) => {
+                let ull = document.getElementById('published-list');
 
-          btnClear.addEventListener('click', (e) => {
-              let list = document.getElementById('published-list');
-              list.innerHTML = '';
-          })
-      }
+                ull.innerHTML = '';
+            })
+            title.value = '';
+            category.value = '';
+            content.value = '';
 
-  }
+        }
 
-  function makeArticleElement(title, category, content) {
+    }
 
-      let article = document.createElement('article');
+    function makeArticleElement(title, category, content) {
 
-      let h4 = document.createElement('h4');
-      h4.textContent = title.value;
-      article.appendChild(h4);
+        let article = document.createElement('article');
 
-      let pCategory = document.createElement('p');
-      pCategory.textContent = `Category: ${category.value}`;
-      article.appendChild(pCategory);
+        let h4 = document.createElement('h4');
+        h4.textContent = title.value;
+        article.appendChild(h4);
 
-      let pContent = document.createElement('p');
-      pContent.textContent = `Content: ${content.value}`;
-      article.appendChild(pContent);
+        let pCategory = document.createElement('p');
+        pCategory.textContent = `Category: ${category.value}`;
+        article.appendChild(pCategory);
 
-      return article;
-  }
+        let pContent = document.createElement('p');
+        pContent.textContent = `Content: ${content.value}`;
+        article.appendChild(pContent);
+
+        return article;
+    }
+
 }
